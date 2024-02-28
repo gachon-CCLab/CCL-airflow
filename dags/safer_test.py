9c7b7e82c47ae144b64f23d2d3a32abbca2fe221
rriakang <kangrira123@gmail.com>
2024-02-28 20:52:49 +0900

    plz

--- dags/safer_test.py
+++ dags/safer_test.py
@@ -4,18 +4,22 @@ import pandas as pd
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-# from airflow.operators.python import BashOperator
+from airflow.operators.python import BashOperator
+from airflow.sensors.filesystem import FileSensor #파일 찾기
 from datetime import datetime, timedelta
 
 
-def Data() :
+
     
-    data = pd.read_csv('/mnt/nas/disk02/Data/Health/Mental_Health/SAFER/20240201/snuh_20240126/snuh_sensing.csv', sep='\t', encoding='utf-8')
-    
-    
+ 
+def Data():
+    url = "http://192.9.202.101:/volume2/디스크02/mnt/nas/disk02/Data/Health/Mental_Health/SAFER/20240201/snuh_20240126/snuh_sensing.csv"
+    response = requests.get(url)
+    data = pd.read_csv(url, sep='\t', encoding='utf-8')
     return data
 
 def Columns (data):
+    
     Column_data =data.columns =['targetId','이름','deviceId','데이터','targetTime']
 
     return Column_data
@@ -50,5 +54,21 @@ Column = PythonOperator(
     dag = dag
 )
 
+t1 = FileSensor(
+    task_id = 'sensor_a',
+    fs_conn_id = 'file_sensor',
+    filepaht='snuh_sensing.csv',
+    dag=dag,
+)
 
-print_starting >> Column
+
+
+check = BashOperator(
+    task_id = 'file_check',
+    bash_command = 'cat /mnt/nas/disk02/Data/Health/Mental_Health/SAFER/20240201/snuh_20240126/snuh_sensing.csv',
+    dag = dag,
+
+)
+
+
+check>>print_starting >> Column
