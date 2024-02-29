e4b80ce7dbebafc8a19066fbe1128ba02bb3e7bf
rriakang <kangrira123@gmail.com>
2024-02-29 11:53:41 +0900

    head

--- dags/safer_test.py
+++ dags/safer_test.py
@@ -7,22 +7,17 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
-
-
-    
- 
 def Data():
     url = "http://192.9.202.101:/디스크02/Data/Health/Mental_Health/SAFER/20240201/snuh_20240126/snuh_sensing.csv"
     encoded_url = urllib.parse.quote(url, safe=':/')
-    
     data = pd.read_csv(encoded_url, sep='\t', encoding='utf-8')
-    print(data)  # 데이터프레임 출력
+    print(data.head())  # 데이터프레임 출력
     return data
 
-def Columns(data):
+def Columns():
+    data = Data()  # 데이터 받아오기
     data.columns = ['targetId', '이름', 'deviceId', '데이터', 'targetTime']  # 열 이름을 변경합니다.
     return data
-
 
 default_args = {
     'owner': 'airflow',
@@ -31,7 +26,6 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-
 dag = DAG(
     'safer_data',
     default_args=default_args,
@@ -39,30 +33,18 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
-
 print_starting = PythonOperator(
-    task_id = 'print_starting',
-    python_callable = Data,
-    provide_context = True,
-    dag = dag,
+    task_id='print_starting',
+    python_callable=Data,
+    provide_context=True,
+    dag=dag,
 )
+
 Column = PythonOperator(
-    task_id = 'Column',
-    op_args=[Data()], #데이터 받아오기
-    python_callable = Columns,
-    provide_context = True,
-    
-    dag = dag
+    task_id='Column',
+    python_callable=Columns,
+    provide_context=True,
+    dag=dag
 )
-
-# t1 = FileSensor(
-#     task_id = 'sensor_a',
-#     fs_conn_id = 'file_sensor',
-#     filepaht='snuh_sensing.csv',
-#     dag=dag,
-# )
-
-
-
 
 print_starting >> Column
