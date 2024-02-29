ddb852eed724d7efbc66a896eacd8da9d95ac02b
rriakang <kangrira123@gmail.com>
2024-02-29 11:27:19 +0900

    col

--- dags/safer_test.py
+++ dags/safer_test.py
@@ -7,7 +7,10 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
-#nas 연결
+
+
+    
+ 
 def Data():
     url = "http://192.9.202.101:/디스크02/Data/Health/Mental_Health/SAFER/20240201/snuh_20240126/snuh_sensing.csv"
     encoded_url = urllib.parse.quote(url, safe=':/')
@@ -15,17 +18,18 @@ def Data():
     data = pd.read_csv(encoded_url, sep='\t', encoding='utf-8')
     return data
 
-def Columns():
-    data = Data()  # Data 함수 호출하여 데이터 읽기
-    Column_data = data.columns = ['targetId', '이름', 'deviceId', '데이터', 'targetTime']
-    return Column_data
+def Columns(data):
+    data.columns = ['targetId', '이름', 'deviceId', '데이터', 'targetTime']  # 열 이름을 변경합니다.
+    return data
+
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024, 2, 27),
+    'start_date': datetime(2024,2,27),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+
 
 dag = DAG(
     'safer_data',
@@ -34,18 +38,28 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
+
 print_starting = PythonOperator(
-    task_id='print_starting',
-    python_callable=Data,
-    provide_context=True,
-    dag=dag,
+    task_id = 'print_starting',
+    python_callable = Data,
+    provide_context = True,
+    dag = dag,
+)
+Column = PythonOperator(
+    task_id = 'Column',
+    python_callable = Columns,
+    provide_context = True,
+    dag = dag
 )
 
-Column = PythonOperator(
-    task_id='Column',
-    python_callable=Columns,
-    provide_context=True,
-    dag=dag,
-)
+# t1 = FileSensor(
+#     task_id = 'sensor_a',
+#     fs_conn_id = 'file_sensor',
+#     filepaht='snuh_sensing.csv',
+#     dag=dag,
+# )
+
+
+
 
 print_starting >> Column
