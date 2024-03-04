a942a9ca6adf950077cae26782a3816de06cbdf2
rriakang <kangrira123@gmail.com>
2024-03-04 16:46:33 +0900

    rr

--- dags/safer_test.py
+++ dags/safer_test.py
@@ -7,18 +7,15 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
-
-
 def Data():
-    url = "http://192.9.202.101:/디스크02/Data/Health/Mental_Health/SAFER/20240201/dumc_20240126/snuh_location.csv"
+    url = "http://192.9.202.101/디스크02/Data/Health/Mental_Health/SAFER/20240201/dumc_20240126/snuh_location.csv"
     data = pd.read_csv(url, sep='\t', encoding='utf-8')
     print(data.head())  # 데이터프레임 출력
     return data
 
-
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024,2,27),
+    'start_date': datetime(2024, 2, 27),
     'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
@@ -37,11 +34,4 @@ print_starting = PythonOperator(
     dag=dag,
 )
 
-# Column = PythonOperator(
-#     task_id='Column',
-#     python_callable=Columns,
-#     provide_context=True,
-#     dag=dag
-# )
-
-print_starting 
+print_starting
