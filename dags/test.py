bc96ce65a96e2c1ba56448eb4975b39f3fd6b106
rriakang <kangrira123@gmail.com>
2024-03-31 22:44:40 +0900

    re

--- dags/test.py
+++ dags/test.py
@@ -1,74 +1,18 @@
-from datetime import datetime, timedelta
-from textwrap import dedent
-
 from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+from sqlalchemy import create_engine
 
-from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
+def query_mysql():
+    # SQLAlchemy 엔진을 사용하여 MySQL 연결 생성
+    engine = create_engine('mysql+pymysql://root:root@210.102.181.208:40011/safer')
+    with engine.connect() as connection:
+        result = pd.read_sql_query('SELECT * FROM crf', con=connection)
+    print(result)
 
-from datetime import datetime 
-def test():
-    print(datetime.now())
-
-    
-with DAG(
-    'tutorial',
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-    },
-    description='A simple tutorial DAG',
-    schedule_interval='1 * * * *',
-    start_date=datetime(2021, 1, 1),
-    catchup=False,
-    tags=['example'],
-) as dag:
-
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    t1 = PythonOperator(
-        task_id='print_date',
-        python_callable=test,
+with DAG('mysql_example_dag', start_date=datetime(2022, 1, 1), schedule_interval='@daily', catchup=False) as dag:
+    query_task = PythonOperator(
+        task_id='query_mysql',
+        python_callable=query_mysql
     )
-
-    t2 = BashOperator(
-        task_id='sleep',
-        depends_on_past=False,
-        bash_command='sleep 5',
-        retries=3,
-    )
-    t1.doc_md = dedent(
-        """\
-    #### Task Documentation
-    You can document your task using the attributes `doc_md` (markdown),
-    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-    rendered in the UI's Task Instance Details page.
-    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-    """
-    )
-
-    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG
-    dag.doc_md = """
-    This is a documentation placed anywhere
-    """  # otherwise, type it like this
-    templated_command = dedent(
-        """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-    {% endfor %}
-    """
-    )
-
-    t3 = BashOperator(
-        task_id='templated',
-        depends_on_past=False,
-        bash_command=templated_command,
-    )
-
-    t1 >> [t2, t3]
